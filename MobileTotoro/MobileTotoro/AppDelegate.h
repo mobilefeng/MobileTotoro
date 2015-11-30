21597b0178ec1bee64666217a158a192aafd09e6
xuyang <mobilefengxy@163.com>
2015-11-30 13:40:46 +0800

    [1]创建工程

---
+++ MobileTotoro/MobileTotoro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MobileTotoro
+//
+//  Created by 徐杨 on 15/11/30.
+//  Copyright © 2015年 xuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MobileTotoro/MobileTotoro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MobileTotoro
+//
+//  Created by 徐杨 on 15/11/30.
+//  Copyright © 2015年 xuyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
